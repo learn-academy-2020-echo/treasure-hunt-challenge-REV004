5807abe68e8e352e0c5122b7c2d4e59be6cd5749
REV004 <rsier004@gmail.com>
2020-11-18 13:40:02 -0800

    added alert that displays the index of the squares

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
-import React, { Component } from 'react'
-import './App.css'
+import React, { Component } from "react";
+import Square from "./components/Square";
+import "./App.css";
 
-class App extends Component{
-  constructor(props){
-    super(props)
-    this.state = {
-      squares: ["?", "?", "?", "?", "?", "?", "?", "?", "?"]
-    }
-  }
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			squares: ["?", "?", "?", "?", "?", "?", "?", "?", "?"],
+		};
+	}
 
-  render(){
-    return(
-      <React.Fragment>
-        <h1>Treasure Hunt App</h1>
-      </React.Fragment>
-    )
-  }
+	handleGamePlay = (index) => {
+		alert(index);
+	};
+
+	render() {
+		return (
+			<React.Fragment>
+				<h1>Treasure Hunt App</h1>
+				<div id='gameboard'>
+					{this.state.squares.map((value, index) => {
+						return (
+							<Square
+								value={value}
+								key={index}
+								index={index}
+								handleGamePlay={this.handleGamePlay}
+							/>
+						);
+					})}
+				</div>
+			</React.Fragment>
+		);
+	}
 }
-export default App
+export default App;
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,11 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-class Square extends Component{
-  render(){
-    return(
-      <React.Fragment>
-      </React.Fragment>
-    )
-  }
+class Square extends Component {
+	handleClick = () => {
+		this.props.handleGamePlay(this.props.index);
+	};
+	render() {
+		return (
+			<React.Fragment>
+				<div id='square' onClick={this.handleClick}>
+					{this.props.value}
+				</div>
+			</React.Fragment>
+		);
+	}
 }
-export default Square
+export default Square;
